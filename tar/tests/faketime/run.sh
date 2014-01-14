e2651ec4b11efb3b04fb0e01818b038c504a89e0
rfree <rfree@gmx.us>
2014-01-14 13:27:03 +0100

    [ok] better test and example .tar

--- tar/tests/faketime/run.sh
+++ tar/tests/faketime/run.sh
@@ -1,3 +1,3 @@
 rm -f test.tar 
 # make && 
-echo "RUN" && ../../src/tar --faketime "2013-01-01 01:00:01" -c -f test.tar EXAMPLEDIR && tar -t -v -f test.tar
+echo "RUN" && ../../src/tar --owner root  --numeric-owner --faketime "2013-01-01 01:00:01" -c -f test.tar EXAMPLEDIR && tar -t -v -f test.tar
