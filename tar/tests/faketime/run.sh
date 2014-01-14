2400b0bac387783c37fecbb7332d9e26c54cd95b
rfree <rfree@gmx.us>
2014-01-14 13:21:58 +0100

    [ok] working test --faketime

--- tar/tests/faketime/run.sh
+++ tar/tests/faketime/run.sh
@@ -1,1 +1,3 @@
-rm test.tar ; make && echo "RUN" && ./src/tar --faketime "2013-01-01 01:00:01" -c -f test.tar EXAMPLEDIR && tar -t -v -f test.tar
+rm -f test.tar 
+# make && 
+echo "RUN" && ../../src/tar --faketime "2013-01-01 01:00:01" -c -f test.tar EXAMPLEDIR && tar -t -v -f test.tar
