64fb4850e10f8f9ec82a9224578712803d131fac
vyrly <vyrly@tormail.org>
2014-01-14 14:23:42 +0100

    [ok] testing appending to tar

--- tar/tests/faketime/run.sh
+++ tar/tests/faketime/run.sh
@@ -1,3 +1,3 @@
 rm -f test.tar 
-# make && 
 echo "RUN" && ../../src/tar --owner root --group root  --numeric-owner --faketime "2013-01-01 01:00:01" -c -f test.tar EXAMPLEDIR && tar -t -v -f test.tar
+echo "RUN-Append" && ../../src/tar --owner root --group root  --numeric-owner --faketime "2013-01-01 01:00:01" -rf test.tar appendexampledir && tar -t -v -f test.tar
