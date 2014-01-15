5eb0f3232566ae4fd7a27b554dc415c8d1739119
sharedaccount <sharedaccount@nowhere.invalid.com>
2014-01-15 06:38:31 +0100

    [fix] correct arguments in wrapper?

--- tar/scripts/install-faketime-local.sh
+++ tar/scripts/install-faketime-local.sh
@@ -1,8 +1,12 @@
+#!/bin/bash -e
+echo "Installing faktime-wrapper to local (user) dir"
  
-mkdir -p ~/.local/usr/bin/
+mkdir -p ~/.local/usr/bin/ 
 mkdir -p ~/.local/usr/lib/faketime-wrapper/
 
 cp -r faketime-wrapper/ ~/.local/usr/lib/
 
 ls -l ~/.local/usr/lib/faketime-wrapper/
+
+echo "Done"
 
