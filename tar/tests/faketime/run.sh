de97731fb7c79673056caa861190fa2e8408c6a4
Jarek <jarek1402b@gmail.com>
2014-01-20 14:01:02 +0100

    add function faketime-reference

--- tar/tests/faketime/run.sh
+++ tar/tests/faketime/run.sh
@@ -1,4 +1,5 @@
 rm -f test.tar 
-echo "RUN FAKETIME-ZERO" && ../../src/tar --owner root --group root  --numeric-owner --faketimezero "" -c -f test.tar EXAMPLEDIR && tar -t -v -f test.tar
+echo "RUN TEST-TIME" && ../../src/tar --owner root --group root  --numeric-owner --faketime-reference "asd.txt" -c -f testt.tar EXAMPLEDIR && tar -t -v -f testt.tar
+echo "RUN FAKETIME-ZERO" && ../../src/tar --owner root --group root  --numeric-owner --faketime-zero "" -c -f test.tar EXAMPLEDIR && tar -t -v -f test.tar
 echo "RUN FAKETIME" && ../../src/tar --owner root --group root  --numeric-owner --faketime "2013-01-01 01:00:01" -c -f test1.tar EXAMPLEDIR && tar -t -v -f test1.tar
-echo "RUN-Append" && ../../src/tar --owner root --group root  --numeric-owner --faketimezero "" -rf test.tar appendexampledir && tar -t -v -f test.tar
+echo "RUN-Append" && ../../src/tar --owner root --group root  --numeric-owner --faketime-zero "" -rf test.tar appendexampledir && tar -t -v -f test.tar
