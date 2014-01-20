4e550873e1ec6cb5a514571d157df7d8703af247
vyrly <vyrly@tormail.org>
2014-01-20 16:03:27 +0100

    [bad] cleanig

--- tar/gnu/chownat.c
+++
@@ -1,5 +0,0 @@
-/* -*- buffer-read-only: t -*- vi: set ro: */
-/* DO NOT EDIT! GENERATED AUTOMATICALLY! */
-#include <config.h>
-#define FCHOWNAT_INLINE _GL_EXTERN_INLINE
-#include "openat.h"
--- tar/gnu/se-selinux.c
+++
@@ -1,5 +0,0 @@
-/* -*- buffer-read-only: t -*- vi: set ro: */
-/* DO NOT EDIT! GENERATED AUTOMATICALLY! */
-#include <config.h>
-#define SE_SELINUX_INLINE _GL_EXTERN_INLINE
-#include <selinux/selinux.h>
